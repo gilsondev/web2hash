8212767a5245e1af20bed264a49738148a752710
Gilson Filho <contatogilsonsbf@gmail.com>
2011-03-29 00:26:40 -0300

    Editing search URL in the Twitter API

--- modules/hashweb2py.py
+++ modules/hashweb2py.py
@@ -7,10 +7,17 @@ from gluon.tools import fetch
 import gluon.contrib.simplejson as json
 
 # URL for searching trend with web2py word    
-WEB2PY_HASH_SEARCH = 'http://search.twitter.com/search.json?q=%23web2py'
+WEB2PY_HASH_SEARCH = 'http://search.twitter.com/search.json?q=web2py'
+
+# Twitter web2py
+USER_WEB2PY = 'http://twitter.com/web2py?format=json'
 
 def search_hashs_web2py():
     page = fetch(WEB2PY_HASH_SEARCH)
     
     return json.loads(page)['results']
     
+def timeline_user_web2py():
+    page = fetch(USER_WEB2PY)
+    
+    return json.loads(page)['#timeline']
