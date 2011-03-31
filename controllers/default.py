c9102d35e4f3a01a041178df032a33c580fac182
Gilson Filho <contatogilsonsbf@gmail.com>
2011-03-31 00:43:35 -0300

    Inserting module FormatTweets in the modules for render links.

--- controllers/default.py
+++ controllers/default.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 # Importing module
-hashw2p = local_import('hashweb2py')
-
-
+import hashweb2py as hashw2p
+import FormatTweets as format
 
 def index():
     """
     Lista all the tweets
     """
     tweets_hash = hashw2p.search_hashs_web2py()
+   
+    for tweet in tweets_hash:
+        tweet_format = format.FormatTweets(tweet['text'])
+        tweet['text'] = tweet_format.format_links()
    
     return dict(tweets_hash=tweets_hash)
 
